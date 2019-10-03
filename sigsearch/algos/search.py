c0d8cb65271193617c77fb757f9e65e70d3ab9d0
Shivank Gautam <shivankgautam98@gmail.com>
2019-10-03 23:24:21 +0530

    Added docstrings of searching algos.

--- sigsearch/algos/search.py
+++ sigsearch/algos/search.py
@@ -2,11 +2,23 @@ from typing import Sequence
 
 
 def linear_search(arr: Sequence[int], x: int) -> int:
-    ...
+    """
+    name: Linear Search
+    link: 'https://en.wikipedia.org/wiki/Linear_search'
+    bst: O(n)
+    avg: O(n)
+    wst: O(n)
+    """
 
 
 def binary_search(arr: Sequence[int], x: int) -> int:
-    ...
+    """
+    name: Binary Search
+    Condition: Sequence must be sorted
+    bst: O(1)
+    avg: O(logn)
+    wst: O(logn)
+    """
 
 
 fns = [linear_search, binary_search]
